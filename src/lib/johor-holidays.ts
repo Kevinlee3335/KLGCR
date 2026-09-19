12330f582fd0b5e0dcd9bfd8c9fa9fcbf586bbd4
Kevinlee3335 <5b8dy8fzbd@privaterelay.appleid.com>
2026-09-19 12:19:48 +0800

    feat: show phone-style Johor calendar

---
+++ src/lib/johor-holidays.ts
@@ -0,0 +1,18 @@
+export type PublicHoliday = { date: string; name: string };
+export const johorPublicHolidays: PublicHoliday[] = [
+  { date: "2026-02-17", name: "Chinese New Year" },
+  { date: "2026-02-18", name: "Chinese New Year Holiday" },
+  { date: "2026-03-20", name: "Hari Raya Aidilfitri" },
+  { date: "2026-03-21", name: "Hari Raya Aidilfitri Holiday" },
+  { date: "2026-03-23", name: "Sultan of Johor's Birthday" },
+  { date: "2026-05-01", name: "Labour Day" },
+  { date: "2026-05-27", name: "Hari Raya Haji" },
+  { date: "2026-05-31", name: "Wesak Day" },
+  { date: "2026-06-01", name: "Yang di-Pertuan Agong's Birthday" },
+  { date: "2026-06-17", name: "Awal Muharram" },
+  { date: "2026-08-25", name: "Maulidur Rasul" },
+  { date: "2026-08-31", name: "National Day" },
+  { date: "2026-09-16", name: "Malaysia Day" },
+  { date: "2026-10-20", name: "Deepavali" },
+  { date: "2026-12-25", name: "Christmas Day" },
+];
