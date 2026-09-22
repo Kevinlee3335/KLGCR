dad62ec5e133f31daaf661b6ba90cbdfc6b13df8
Kevinlee3335 <5b8dy8fzbd@privaterelay.appleid.com>
2026-09-22 14:55:24 +0800

    Enable background mobile push notifications

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,13 @@
+self.addEventListener("push", (event) => {
+  const data = event.data ? event.data.json() : {};
+  event.waitUntil(self.registration.showNotification(data.title || "KLGCR notification", {
+    body: data.body || "You have a new KLGCR notification.",
+    icon: "/klg-campus-residence-logo.png",
+    badge: "/klg-campus-residence-logo.png",
+    data: { href: data.href || "/" },
+  }));
+});
+
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
   const href = event.notification.data?.href || "/";
